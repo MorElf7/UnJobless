34065d56bc7edff57e1e17fea54710e39002dd46
kien-to <ktto@umass.edu>
2024-04-09 20:27:02 -0400

    connect mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { MONGO_SECRET } from 'config/index';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [UserModule, MongooseModule.forRoot(MONGO_SECRET)],
   controllers: [AppController],
   providers: [AppService],
 })
