c5124b1d2e0f3b2eee492f3a321d5b4b82da7f09
Dang Tran <danqtran@umass.edu>
2024-04-14 00:22:59 -0400

    Almost fixed Navbar styling

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,59 +1,3 @@
-// import React, { useState } from "react";
-// import Link from 'next/link';
-// import styles from "../styles/Navbar.module.scss";
-// import "boxicons/css/boxicons.min.css";
-
-// interface Props {
-//   isCollapsed: boolean;
-//   handleCollapse: (arg: boolean) => void;
-// }
-
-// const Navbar = ({ isCollapsed, handleCollapse }: Props) => {
-//   const items = ["Dashboard", "Jobs", "Applications", "Profile"];
-//   const icons = ["grid-alt", "briefcase", "paper-plane", "user"];
-//   const refs = ["/", "/jobs", "/applications", "/profile"];
-//   const [collapse, setCollapse] = useState(isCollapsed);
-//   const [activeItem, setActiveItem] = useState(0);
-
-//   const toggleSidebar = () => {
-//     setCollapse(!collapse);
-//     handleCollapse(!collapse);
-//   };
-
-//   return (
-//     <div className={`${styles.navbar} ${!collapse ? styles.open : ''}`}>
-//       <div className={styles.logoDetails}>
-//         <i className='bx bxs-cat icon'></i>
-//         <div className={styles.logoName}>UnjoBless</div>
-//         <i className='bx bx-menu' id="btn" onClick={toggleSidebar}></i>
-//       </div>
-//       <ul className={styles.navList}>
-//         {items.map((item, id) => (
-//           <li key={id} className={`${activeItem === id ? styles.active : ''}`} onClick={() => setActiveItem(id)}>
-//             <Link href={refs[id]} className={styles.link}>
-//               <span className="link-content">
-//                 <i className={`bx bx-${icons[id]}`}></i>
-//                 <span className={styles.linksName}>{item}</span>
-//               </span>
-//             </Link>
-//             <span className={styles.tooltip}>{item}</span>
-//           </li>
-//         ))}
-//         <li className={styles.profile}>
-//           <div className={styles.profileDetails}>
-//             <img src="profile.jpg" alt="profileImg" />
-//             <span className={styles.name}>Real Name</span>
-//           </div>
-//           <i className='bx bx-log-out' id="log_out"></i>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
-
 import React, { useState } from "react";
 import Link from 'next/link';
 import styles from "../styles/Navbar.module.scss";
@@ -80,14 +24,14 @@ const Navbar = ({ isCollapsed, handleCollapse }: Props) => {
     <div className={styles.global}>
       <div className={`${styles.navbar} ${!collapse && styles.open}`}>
         <div className={styles.logoDetails}>
-          <i className='bx bxs-cat icon'></i>
+          <i className={`bx bxs-cat ${styles.icon}`}></i>
           <div className={styles.logoName}>UnjoBless</div>
-          <i className='bx bx-menu' id="btn" onClick={toggleSidebar}></i>
+          <i className={`bx bx-menu ${styles.btn}`} onClick={toggleSidebar}></i>
         </div>
         <ul className={styles.navList}>
           <div>
             {items.map((item, id) => (
-              <li key={id} className={`${activeItem === id && styles.active}`} onClick={() => setActiveItem(id)}>
+              <li key={id} className={`${styles.linkItem} ${activeItem === id && styles.active}`} onClick={() => setActiveItem(id)}>
                 <Link href={refs[id]} className={styles.link}>
                   <i className={`bx bx-${icons[id]}`}></i>
                   <span className={styles.linksName}>{item}</span>
@@ -97,12 +41,12 @@ const Navbar = ({ isCollapsed, handleCollapse }: Props) => {
             ))}
           </div>
           <div>
-            <li className={styles.profile}>
+            <li className={`${styles.linkItem} ${styles.profile}`}>
               <div className={styles.profileDetails}>
                 <img src="profile.jpg" alt="profileImg" />
                 <span className={styles.name}>Real Name</span>
               </div>
-              <i className='bx bx-log-out' id="log_out" ></i>
+              <i className={`bx bx-log-out ${styles.logOut}`} ></i>
             </li>
           </div>
         </ul>
