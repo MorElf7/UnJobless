6d33c53ef10fe40f205ef861c1da55e962b0e325
Quang Dang <qdang@umass.edu>
2024-04-21 00:10:24 -0400

    Added default App page

---
+++ apps/web/src/pages/Applications.tsx
@@ -0,0 +1,11 @@
+const Applications = () => {
+    return (
+        <div className="dashboard">
+            <div className="dashboardContainer">
+                <span color="smoke">Apps NPC</span>
+            </div>
+        </div>
+    );
+};
+
+export default Applications;
--- apps/web/src/routes/Router.tsx
+++ apps/web/src/routes/Router.tsx
@@ -3,6 +3,7 @@ import MainLayout from "../layouts/MainLayout";
 import Dashboard from "../pages/Dashboard";
 import Jobs from "../pages/Jobs";
 import { Profile } from "../pages/Profile";
+import Applications from "../pages/Applications";
 
 function Router() {
     return (
@@ -11,6 +12,7 @@ function Router() {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/jobs" element={<Jobs />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/applications" element={<Applications />} />
             </Route>
         </Routes>
     );
