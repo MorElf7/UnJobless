a35eb4e4677c71ae2ee52a2e52749e7f8a389746
kien-to <ktto@umass.edu>
2024-05-01 13:48:30 -0400

    things

--- config/dev.config.js
+++ config/dev.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  // CLIENT_ID: '161022891404-ghnk59917kanudg50c0uiv545tfp796p.apps.googleusercontent.com',
-  // CLIENT_SECRET: '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',
-  // SMTP_EMAIL: 'kiento952002@gmail.com',
-  // REFRESH_TOKEN: '1//04HHBegEpQw2GCgYIARAAGAQSNwF-L9IrZlc0nsUvwsDeNQ9NebjG8QAs7iSMGVybCqVSIL57CJBi8Pl87_M_5bUNt5qUkddJAak',
   MONGO_SECRET: 'mongodb+srv://cs520:string123@cluster520.yytecem.mongodb.net/',
   OPENAI_API_KEY: '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',
   // 'mongodb+srv://cs320-geocities:teamgeocities@cluster320.ndeefoh.mongodb.net/?retryWrites=true&w=majority',
